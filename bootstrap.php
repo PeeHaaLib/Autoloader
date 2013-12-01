03a7c2ede3a454f9281e91b15f029cc3327cf726
PeeHaa <info@pieterhordijk.com>
2013-12-01 21:46:05 +0100

    Added bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This bootstraps the PeeHaaLib Autoloader library
+ *
+ * PHP version 5.4
+ *
+ * @category   PeeHaaLib
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    0.0.1
+ */
+namespace PeeHaaLib;
+
+/**
+ * Bootstrap the Autoloader library
+ */
+require_once __DIR__ . '/src/PeeHaaLib/Autoloader/Psr0.php';
